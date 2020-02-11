ac3c4a26547f58ce15379d52d3f67a89af5c813c
Dalton Eschberger <eschberger.dalton@gmail.com>
2020-02-11 13:43:51 -0600

    init

--- NeuralNetwork_tester.py
+++ NeuralNetwork_tester.py
@@ -1,26 +1,31 @@
 import numpy as np
-from NeuralNetwork import NeuralNetwork
 
-        
+from NeuralNetwork import NeuralNetwork
+from Finalize import Finalize
 
 if __name__=='__main__':
-    training_inputs = np.array([[0,0,1],
-                                [1,1,1],
+    training_inputs = np.array([[0,0,0],
+                                [0,0,1],
+                                [0,1,0],
+                                [0,1,1],
+                                [1,0,0],
                                 [1,0,1],
-                                [0,1,1]])
-    training_outputs = np.array([[0,1,1,0]]).T
+                                [1,1,0],
+                                [1,1,1]])
+
+    training_outputs = np.array([[0,0,1,1,0,0,1,1]]).T
     print("Input Data:")
-    for i in range(4):
+    for i in range(8):
         print(training_inputs[i],training_outputs[i])
 
     network = NeuralNetwork()
-    print(network.synaptic_weights)
+    
     network.train(training_inputs, training_outputs, 10000)
-    print(network.synaptic_weights)
-    A = str(input("Input 1:"))
-    B = str(input("Input 1:"))
-    C = str(input("Input 1:"))
 
-    print("New Situation: Input = ", A,B,C)
-    print("Output Data:")
-    print(network.think(np.array([A,B,C])))
+    A = str(input("Input 1:"))
+    B = str(input("Input 2:"))
+    C = str(input("Input 3:"))
+
+    output=Finalize(network.think(np.array([A,B,C])))
+    
+    print("Answer is: ", output.final, " with ", output.percent,"% certainty")
